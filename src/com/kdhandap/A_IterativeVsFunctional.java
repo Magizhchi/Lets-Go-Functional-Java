0293d25a48112f3a0e6738c823ab01fd22e71031
Karthikk <karthikk296d@gmail.com>
2016-09-21 23:33:48 -0500

    A Basic Test Case

--- src/com/kdhandap/A_IterativeVsFunctional.java
+++ src/com/kdhandap/A_IterativeVsFunctional.java
@@ -1,7 +1,16 @@
 package com.kdhandap;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class A_IterativeVsFunctional {
+    /* Lets now compare the traditional iterative and the much nicer functional way of java
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(factorsOf(1));
+    }
+
+    private static List<Integer> factorsOf(int i) {
+        return Arrays.asList(1);
     }
 }
