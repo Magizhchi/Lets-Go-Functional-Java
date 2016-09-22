0893d31022e442ee6b237ec6ead68879af9eb9f9
Karthikk <karthikk296d@gmail.com>
2016-09-21 23:59:34 -0500

    Functional Way

--- src/com/kdhandap/A_IterativeVsFunctional.java
+++ src/com/kdhandap/A_IterativeVsFunctional.java
@@ -1,13 +1,34 @@
 package com.kdhandap;
 
 import java.util.*;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 
 public class A_IterativeVsFunctional {
     /* Lets now compare the traditional iterative and the much nicer functional way of java
      */
     public static void main(String[] args) {
+        // Given a list of numbers, return the list of all faactors
+
         List<Integer> numbers = Arrays.asList(2,5,17,16,21);
-        System.out.println("Iterative" + factorsOfIterative(numbers));
+        System.out.println("Iterative  :" + factorsOfIterative(numbers));
+        System.out.println("Functional :" + factorsOfRecursive(numbers));
+    }
+
+    private static Set<Integer> factorsOfRecursive(List<Integer> numbers) {
+        return numbers.stream()
+                      .flatMap(e -> factorsOfRecursive(e).stream())
+                      .collect(toSet());
+    }
+
+
+    private static List<Integer> factorsOfRecursive(int number) {
+        return IntStream.rangeClosed(1, number)
+                        .filter( i -> number % i == 0)
+                        .boxed()
+                        .collect(toList());
     }
 
     private static Set<Integer> factorsOfIterative(List<Integer> numbers) {
