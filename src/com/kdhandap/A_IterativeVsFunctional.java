e7d82b85c449c0e7c36c7f71de92eb083f4be0b3
Karthikk <karthikk296d@gmail.com>
2016-09-21 23:52:25 -0500

    Iterative Way

--- src/com/kdhandap/A_IterativeVsFunctional.java
+++ src/com/kdhandap/A_IterativeVsFunctional.java
@@ -1,16 +1,29 @@
 package com.kdhandap;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class A_IterativeVsFunctional {
     /* Lets now compare the traditional iterative and the much nicer functional way of java
      */
     public static void main(String[] args) {
-        System.out.println(factorsOf(1));
+        List<Integer> numbers = Arrays.asList(2,5,17,16,21);
+        System.out.println("Iterative" + factorsOfIterative(numbers));
     }
 
-    private static List<Integer> factorsOf(int i) {
-        return Arrays.asList(1);
+    private static Set<Integer> factorsOfIterative(List<Integer> numbers) {
+        Set<Integer> factors = new HashSet<>();
+        for (int number : numbers){
+            factors.addAll(factorsOfIterative(number));
+        }
+        return factors;
+    }
+
+    private static List<Integer> factorsOfIterative(int number) {
+        List<Integer> factors = new ArrayList<>();
+        for (int j = 1; j <= number; j++) {
+            if (number % j == 0)
+                factors.add(j);
+        }
+        return factors;
     }
 }
