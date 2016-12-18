dfc1d7d85a1a5163b8d8f70b1c161142fc044d8e
Karthikk <karthikk296d@gmail.com>
2016-12-17 21:53:34 -0600

    Primitive Array - Functional Programming for caveMen

--- src/com/kdhandap/J_Primitive_Array.java
+++ src/com/kdhandap/J_Primitive_Array.java
@@ -1,7 +1,22 @@
 package com.kdhandap;
 
+import java.io.File;
+import java.util.stream.Stream;
+
 public class J_Primitive_Array {
+    /*
+    In this Example we will be seeing how create a stream out of primitive arrays and convert them to a functional Style
+    For this we will be using the of() method in Stream
+     */
     public static void main(String[] args) {
-        System.out.println("Hwllo");
+        File file = new File("/Users/karthikk");
+
+        File[] children = file.listFiles();
+
+        // The null check here is a necessary evil.
+        if (children != null){
+            Stream.of(children)
+                    .forEach(System.out::println);
+        }
     }
 }
